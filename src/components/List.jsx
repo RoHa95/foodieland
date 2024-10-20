924758cc6205a98e18cdae0062b9282b4501c16d
Roqhaye Halimi <roqiahalimi95@gmail.com>
2024-10-20 23:34:17 +0330

    completed list section and card component

---
+++ src/components/List.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useFoods } from "../context/FoodsContext";
+import Card from "./Card";
+function List() {
+    const foods = useFoods();
+    console.log(foods);
+    
+  return (
+    <div className="w-full flex flex-col container mx-auto 2xl:max-w-7xl 2xl:mx-auto px-24 mb-24">
+      <div className=" flex flex-col items-center justify-center gap-y-6 mb-24">
+        <h3 className=" text-[48px] font-semibold">Simple and tasty recipes</h3>
+        <p className=" font-normal text-base text-opacity-60 w-[706px] text-center">
+          Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqut enim ad minim{" "}
+        </p>
+      </div>
+      <div className=" w-full grid grid-cols-3 gap-x-5 gap-y-8">
+        {foods.map(item=>(
+            <Card data={item}/>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default List;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Banner from '../components/Banner'
 import Categories from '../components/Categories'
+import List from '../components/List'
 
 function Home() {
   return (
@@ -9,6 +10,7 @@ function Home() {
     <Navbar/>
     <Banner/>
     <Categories/>
+    <List/>
     </>
     
     
