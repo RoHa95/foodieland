688146266dd33f2bb99869c7507b64d6911aafd4
Roqhaye Halimi <roqiahalimi95@gmail.com>
2024-11-01 19:05:43 +0330

    responsived List2 component

--- src/components/List2.jsx
+++ src/components/List2.jsx
@@ -14,17 +14,17 @@ function List2() {
   const navigate = useNavigate();
 
   return (
-    <div className="w-full flex flex-col container mx-auto 2xl:max-w-7xl 2xl:mx-auto px-24 mb-24">
+    <div className="w-full flex flex-col container mx-auto 2xl:max-w-7xl 2xl:mx-auto px-10 md:px-24 mb-24">
       <div className=" flex flex-col md:flex-row items-center justify-between w-full mb-20">
-        <div className=" w-1/2 text-[48px] font-semibold">
+        <div className=" w-full md:w-1/2 text-3xl lg:text-[48px] lg:leading-[56px] font-semibold">
           Try this delicious recipe to make your day
         </div>
-        <div className=" w-1/2 font-normal text-base text-opacity-60">
+        <div className=" w-full md:w-1/2 font-normal text-base text-opacity-60">
           Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod
           tempor incididunt ut labore et dolore magna aliqut enim ad minim{" "}
         </div>
       </div>
-      <div className=" w-full grid grid-cols-4">
+      <div className=" w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {foods.length === 0 ? (
           <div className=" col-span-full flex justify-center items-center">
             <Bars color="#0ea5e9" />
